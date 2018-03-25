e311477ab481ff2777427387de7c7cea46cf76e4
Enmanuel Medina <enmanuelm19@gmail.com>
2018-03-24 21:10:36 -0300

    Adding testing suite

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,29 +1,21 @@
 class VideosController < ApplicationController
   before_action :set_video, only: [:show, :edit, :update, :destroy]
 
-  # GET /videos
-  # GET /videos.json
   def index
     @videos = Video.all
   end
 
-  # GET /videos/1
-  # GET /videos/1.json
   def show
     @videos = Video.all
   end
 
-  # GET /videos/new
   def new
     @video = Video.new
   end
 
-  # GET /videos/1/edit
   def edit
   end
 
-  # POST /videos
-  # POST /videos.json
   def create
     @video = Video.new(video_params)
 
@@ -38,8 +30,6 @@ class VideosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /videos/1
-  # PATCH/PUT /videos/1.json
   def update
     respond_to do |format|
       if @video.update(video_params)
@@ -52,8 +42,6 @@ class VideosController < ApplicationController
     end
   end
 
-  # DELETE /videos/1
-  # DELETE /videos/1.json
   def destroy
     @video.destroy
     respond_to do |format|
@@ -63,12 +51,10 @@ class VideosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_video
       @video = Video.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def video_params
       params.require(:video).permit(:wistia, :description, :title)
     end
